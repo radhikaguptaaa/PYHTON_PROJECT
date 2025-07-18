4de1d417d4340898ff537dd887a5fec47abb13f7
radhikaguptaaa <radhikaaaguptaa36@gmail.com>
2025-07-18 13:36:59 +0530

    i commit

--- 03.VirtualAssistant/main.py
+++ 03.VirtualAssistant/main.py
@@ -1,89 +1,113 @@
 import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+import requests
+# from openai import OpenAI
+from gtts import gTTS
+import pygame
+import os
 
-# import webbrowser
-# import pyttsx3
-# import musicLibrary
-# import requests
 
-# recognizer = sr.Recognizer()
-# engine = pyttsx3.init()
-# newsapi = "47e7a7affcf14054a77ecfa23e10ce48"
 
-# def speak(text):
-#     engine.say(text)
-#     engine.runAndWait()
+recognizer = sr.Recognizer()
+engine = pyttsx3.init() 
+newsapi = "47e7a7affcf14054a77ecfa23e10ce48"
 
-# def processCommand(c):
-#     if "open google" in c.lower():
-#         webbrowser.open("https://google.com")
-#     elif "open youtube" in c.lower():
-#         webbrowser.open("https://youtube.com")
-#     elif "open facebook" in c.lower():
-#         webbrowser.open("https://facebook.com")
-#     elif "open instagram" in c.lower():
-#         webbrowser.open("https://instagram.com")
-#     elif c.lower().startswith("play"):
-#         song = c.lower().split(" ")[1]
-#         link = musicLibrary.music.get(song)
-#         if link:
-#             webbrowser.open(link)
-#         else:
-#             speak("Song not found.")
+def speak_old(text):
+    engine.say(text)
+    engine.runAndWait()
 
-#     elif "news" in c.lower():
-#         r=requests.get("https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=47e7a7affcf14054a77ecfa23e10ce48") 
-#         if r.status_code == 200:
-#             data = r.json() #Parse the json response 
-#             articles = data.get('articles', [])
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
 
-#             #print headlines 
-#             for x in articles:
-#                 speak(x['title'])   
-#     else:
-#         #let open AI handle the request
-#         pass             
+    # Initialize Pygame mixer
+    pygame.mixer.init()
 
+    # Load the MP3 file
+    pygame.mixer.music.load('temp.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
     
-    
-    
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3") 
 
-        
+# def aiProcess(command):
+#     client = OpenAI(api_key="<Your Key Here>",
+#     )
+
+#     completion = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud. Give short responses please"},
+#         {"role": "user", "content": command}
+#     ]
+#     )
+
+#     return completion.choices[0].message.content
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+
+    elif "news" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey={newsapi}")
+        if r.status_code == 200:
+            # Parse the JSON response
+            data = r.json()
+            
+            # Extract the articles
+            articles = data.get('articles', [])
+            
+            # Print the headlines
+            for article in articles:
+                speak(article['title'])
+
+    else:
+        pass
+        # Let OpenAI handle the request
+        # output = aiProcess(c)
+        # speak(output) 
+
+if __name__ == "__main__" :
+    speak("Initializing ROBO...")
+    while True:
+        # Listen for the wake word "Jarvis"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+         
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=3, phrase_time_limit=10)
+            word = r.recognize_google(audio)
+            if(word.lower() == "robo"):
+                speak("Ya")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Robo Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
 
 
-# if __name__ == "__main__":
-#     speak("Initialising Robo....")
-#     #Listen for the word robo to start
-#     while True:
-#         #Obtain audio from microphone
-#         r = sr.Recognizer()
-        
-
-#         #recognize speech 
-#         try:
-#             with sr.Microphone() as source :
-#                 print("Listening ....")
-#                 audio = r.listen(source , timeout= 2 ,  phrase_time_limit = 1)
-#             word = r.recognize_google(audio)
-#             print(word)
-#             if(word.lower() == "robo"):
-#                 speak("yes , how can I help you")
-#                 #Listen for command
-#                 with sr.Microphone() as source:
-#                     print("Robo active")
-#                     audio = r.listen(source)
-#                     command = r.recognize_google(audio)
-
-
-#                     processCommand(command)
-#         except Exception as e:
-#             print(e)    
-#         except sr.WaitTimeoutError:
-#             print("Timeout. No speech detected.")        
-
-
-
-
-
-
-
-
+        except Exception as e:
+            print("Error; {0}".format(e)) 
