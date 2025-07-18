ade0e9136c76fe4ef1f84fed74035590b803db4a
radhikaguptaaa <radhikaaaguptaa36@gmail.com>
2025-07-18 12:35:00 +0530

    i coomit

---
+++ 03.VirtualAssistant/musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "paro" : "https://youtu.be/Es4NrOnoNb4?si=zN7jZ4BMwggiYg2V",
+    "jab tak hai jaan" : "https://youtu.be/EnrY5LinNH0?si=PTOcKMJ_7gbOF1y6",
+    "saibo" : "https://youtu.be/GtNrQy90Ih4?si=m_2b8_5aHFtPI5xB",
+    "chikni chameli": "https://youtu.be/MQM7CNoAsBI?si=aUA1RnHFQuw1UYkZ",
+    "kajra re" : "https://youtu.be/4dsFQFCvVGU?si=o0H9YcKf7YnG4vKz",
+    "billo rani" : "https://youtu.be/T15qhmhqraE?si=vPefN7jd2RSIA20p"
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PYHTON_PROJECT
+In this I have created a few simple projects using python 
+01 - CLASSIC SNAKE , WATER AND GUN GAME
+02 - GUESS THE NUMBER 
